7caaab8e419204fcf5cc4d3b427b89c53e392e9c
André Ricardo <contatoadmx@gmail.com>
2023-04-03 03:47:54 -0300

    fx redirect

--- class/Middleware/MdEnergize.php
+++ class/Middleware/MdEnergize.php
@@ -4,6 +4,7 @@ namespace Middleware;
 
 use Closure;
 use Elegance\Energize;
+use Elegance\Request;
 use Elegance\Response;
 use Error;
 use Exception;
@@ -15,10 +16,12 @@ class MdEnergize
         try {
             return Energize::solve($next());
         } catch (Error | Exception $e) {
-            if ($e->getCode() == STS_REDIRECT) {
-                Response::cache(false);
-                Response::header('New-Location', $e->getMessage());
-                throw new Exception('', STS_OK);
+            if (Request::header('Energize-Request-Type')) {
+                if ($e->getCode() == STS_REDIRECT) {
+                    Response::cache(false);
+                    Response::header('New-Location', $e->getMessage());
+                    throw new Exception('', STS_OK);
+                }
             }
             throw $e;
         }
--- helper/function/redirect.php
+++ helper/function/redirect.php
@@ -1,13 +1,10 @@
 <?php
 
-use Elegance\Response;
-
 if (!function_exists('redirect')) {
 
     /** Lança uma exception de redirecionamento */
     function redirect(): never
     {
-        $url = url(...func_get_args());
         throw new Exception(url(...func_get_args()), STS_REDIRECT);
     }
 }
