de748c1a07fb85e4cc5492d8b4ec46e341a16d5f
André Ricardo <contatoadmx@gmail.com>
2023-04-05 18:17:29 -0300

    auto get project favicon

--- helper/script/router.php
+++ helper/script/router.php
@@ -1,9 +1,22 @@
 <?php
 
 use Elegance\Assets;
+use Elegance\File;
+use Elegance\Response;
 use Elegance\Router;
 
 Router::exFriendlyRoute('php');
 Router::exFriendlyRoute('html');
 
-Router::get('favicon.ico', fn () => Assets::send(dirname(__DIR__, 2) . "/library/assets/favicon.ico"));
+Router::get('favicon.ico', function () {
+    $file = 'library/assets/favicon.ico';
+
+    if (File::check($file)) {
+        Assets::load($file);
+    } else {
+        Assets::load(dirname(__DIR__, 2) . "/$file");
+        Response::cache(false);
+    }
+
+    Response::send();
+});
