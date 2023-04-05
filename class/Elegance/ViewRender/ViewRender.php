9a917f4d35b585f4d847bb59c4f12958c5f9de87
André Ricardo <contatoadmx@gmail.com>
2023-04-05 11:25:03 -0300

    default params in view render

--- class/Elegance/ViewRender/ViewRender.php
+++ class/Elegance/ViewRender/ViewRender.php
@@ -7,5 +7,5 @@ use Elegance\View;
 abstract class ViewRender extends View
 {
     /** Aplica ações extras ao renderizar uma view */
-    abstract protected static function renderizeAction(string $content, array $params): string;
+    abstract protected static function renderizeAction(string $content, array $params = []): string;
 }
