266fb9b5210f6d6ab1f880da58f72be95af4fd3c
André Ricardo <contatoadmx@gmail.com>
2023-04-05 05:02:03 -0300

    revised default routes

--- helper/script/router.php
+++ helper/script/router.php
@@ -1,21 +1,6 @@
 <?php
 
 use Elegance\Assets;
-use Elegance\File;
 use Elegance\Router;
 
-Router::get('favicon.ico', function () {
-    $file = 'library/assets/favicon.ico';
-
-    if (!File::check($file)) $file = dirname(__DIR__, 2) . "/$file";
-
-    Assets::send($file);
-});
-
-Router::get('front.js', function () {
-    $file = 'library/assets/front.js';
-
-    if (!File::check($file)) $file = dirname(__DIR__, 2) . "/$file";
-
-    Assets::send($file);
-});
+Router::get('favicon.ico', fn () => Assets::send(dirname(__DIR__, 2) . "/library/assets/favicon.ico"));
