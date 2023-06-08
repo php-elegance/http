3546dc5735a8560c09a0c79a050202ea07b85b0a
Andre Ricardo <contatoadmx@gmail.com>
2023-06-07 22:29:45 -0300

    fix install.structure

--- source/class/Command/Install/MxStructure.php
+++ source/class/Command/Install/MxStructure.php
@@ -12,22 +12,22 @@ abstract class MxStructure
     {
         $basePath = dirname(__DIR__, 4);
 
-        Dir::create('teste/library');
-        Dir::create('teste/library/assets');
-        Dir::create('teste/source/class');
-        Dir::create('teste/source/helper');
-        Dir::create('teste/source/helper/constant');
-        Dir::create('teste/source/helper/function');
-        Dir::create('teste/source/helper/script');
-        Dir::create('teste/view');
-        Dir::create('teste/vue');
+        Dir::create('library');
+        Dir::create('library/assets');
+        Dir::create('source/class');
+        Dir::create('source/helper');
+        Dir::create('source/helper/constant');
+        Dir::create('source/helper/function');
+        Dir::create('source/helper/script');
+        Dir::create('view');
+        Dir::create('vue');
 
         MxCmd::echo('Estrutura criada');
 
-        File::copy("$basePath/library/assets/front.js", 'teste/library/assets/front.js');
-        File::copy("$basePath/library/base.html", 'teste/library/base.html');
-        File::copy("$basePath/source/class/Middleware/Response/MdApi.php", 'teste/source/class/Middleware/Response/MdApi.php');
-        File::copy("$basePath/source/class/Middleware/Response/MdFront.php", 'teste/source/class/Middleware/Response/MdFront.php');
+        File::copy("$basePath/library/assets/front.js", 'library/assets/front.js');
+        File::copy("$basePath/library/base.html", 'library/base.html');
+        File::copy("$basePath/source/class/Middleware/Response/MdApi.php", 'source/class/Middleware/Response/MdApi.php');
+        File::copy("$basePath/source/class/Middleware/Response/MdFront.php", 'source/class/Middleware/Response/MdFront.php');
 
         MxCmd::echo('Arquivos principais instalados');
 
