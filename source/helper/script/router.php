f2c0a5bec4c241cfa4506cf40caf38f5a8ca5bd2
Andre Ricardo <contatoadmx@gmail.com>
2023-06-13 22:59:32 -0300

    return vuejs router

--- source/helper/script/router.php
+++ source/helper/script/router.php
@@ -17,3 +17,15 @@ Router::add('favicon.ico', function () {
 
     Response::send();
 });
+
+Router::add('vue.js', function () {
+    $file = dirname(__DIR__, 3) . '/library/assets/vue.js';
+
+    Assets::load($file);
+
+    Response::cache(730);
+
+    Response::send();
+
+    //Copia de https://unpkg.com/vue@3.2.47/dist/vue.global.prod.js
+});
